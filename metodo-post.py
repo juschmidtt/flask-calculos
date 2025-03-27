58629341f5a4540a71ff45feabade012d1f60a40
Júlia Schmidt <141142651+juschmidtt@users.noreply.github.com>
2025-03-27 14:01:27 -0300

    metodo delete

--- metodo-post.py
+++ metodo-post.py
@@ -11,10 +11,22 @@ def somar():
     numero2 = dados_recebidos["numero2"]
     resultado = numero1 + numero2
     
-    historico.append({"numero1": numero1, "numero2": numero2, "resultado": resultado})
+    historico.append({
+        "id": str(uuid.uuid4()),
+        "numero1": numero1, 
+        "numero2": numero2, 
+        "resultado": resultado})
     
     return {'resultado': resultado}
 
 @app.route("/calculos", methods=["GET"])
 def calculos():
     return {'historico':historico}
+
+
+@app.route("/deletar/<id>", methods=["DELETE"])
+def deletar_item(id):
+    global lista_calculos
+    lista_calculos = [d for d in lista_calculos if d.get ('id') != id]
+
+    return{}
