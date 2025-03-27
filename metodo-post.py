a644c616e225c6c2e0abd2b88eb28f2684f3db22
Júlia Schmidt <141142651+juschmidtt@users.noreply.github.com>
2025-03-27 14:41:48 -0300

    atualização com put 2.0

--- metodo-post.py
+++ metodo-post.py
@@ -31,3 +31,24 @@ def deletar_item(id):
     historico = [d for d in historico if d.get ('id') != id]
 
     return{}
+
+@app.route("/editar/<id>", methods=["PUT"])
+def editar_item(id):
+    global historico
+    dados_recebidos = request.get_json()
+    numero1 = dados_recebidos["numero1"]
+    numero2 = dados_recebidos["numero2"]
+
+    lista_temp = []
+    for calculo in historico:
+        if calculo['id'] == id: 
+            resultado = numero1 + numero2
+            historico.append({
+            "id": str(uuid.uuid4()),
+            "numero1": numero1, 
+            "numero2": numero2, 
+            "resultado": resultado})
+        else: 
+            lista_temp.append(calculo)
+    historico = lista_temp
+    return{}        
